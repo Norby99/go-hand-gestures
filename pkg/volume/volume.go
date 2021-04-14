bd5c5bf0f38b5cd1a1da9d111d893c1984447f40
Diego <11472973+UnwishingMoon@users.noreply.github.com>
2021-04-14 20:49:30 +0200

    Added functions in volume package

--- pkg/volume/volume.go
+++ pkg/volume/volume.go
@@ -1,6 +1,32 @@
 package volume
 
-type Audio struct {
-	MicVolume      uint // 0-100
-	SpeakersVolume uint // 0-100
+import "errors"
+
+// SystemAudio represents the system volume
+type SystemAudio struct {
+	ID             int
+	MicVolume      int // 0-100
+	IsMicMuted     bool
+	SpeakerVolume  int // 0-100
+	IsSpeakerMuted bool
+}
+
+func (r *SystemAudio) IncreaseSpeaker(inc int) (int, error) {
+	if outOfRange(inc) {
+		return r.SpeakerVolume, errors.New("")
+	}
+
+	return 1, nil
+}
+
+func (r *SystemAudio) DecreaseSpeaker(inc int) (int, error) {
+	if outOfRange(inc) {
+		return r.SpeakerVolume, errors.New("")
+	}
+
+	return 1, nil
+}
+
+func outOfRange(val int) bool {
+	return val < 0 || val > 100
 }
