a9955128cf385dabe4ca932579e3404641911400
Diego <11472973+UnwishingMoon@users.noreply.github.com>
2021-04-19 19:07:30 +0200

    Created speakers volume functions

--- pkg/volume/volume.go
+++ pkg/volume/volume.go
@@ -1,30 +1,112 @@
+// +build !windows,!darwin
+
 package volume
 
-import "errors"
+import (
+	"errors"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
-// SystemAudio represents the system volume
-type SystemAudio struct {
-	ID             int
-	MicVolume      int // 0-100
-	IsMicMuted     bool
-	SpeakerVolume  int // 0-100
-	IsSpeakerMuted bool
+var useAmixer = false
+var pattern = regexp.MustCompile(`\d+%`)
+
+func execCmd(args []string) ([]byte, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, errors.New("Could not execute command" + strings.Join(args, " "))
+	}
+	return out, nil
 }
 
-func (r *SystemAudio) IncreaseSpeaker(inc int) (int, error) {
-	if outOfRange(inc) {
-		return r.SpeakerVolume, errors.New("")
-	}
+func init() {
+	useAmixer = false
 
-	return 1, nil
+	if _, err := execCmd([]string{"pactl", "info"}); err != nil {
+		useAmixer = true
+	}
 }
 
-func (r *SystemAudio) DecreaseSpeaker(inc int) (int, error) {
-	if outOfRange(inc) {
-		return r.SpeakerVolume, errors.New("")
+func getVolumeCmd() []string {
+	if useAmixer {
+		return []string{"amixer", "get", "Master"}
 	}
+	return []string{"pactl", "list", "sinks"}
+}
 
-	return 1, nil
+// GetSpeakerVolume Get the volume of the main speaker
+func GetSpeakerVolume() (int, error) {
+	out, err := execCmd(getVolumeCmd())
+	if err != nil {
+		return 0, err
+	}
+	return parseVolume(string(out))
+}
+
+func setVolumeCmd(volume int) []string {
+	if useAmixer {
+		return []string{"amixer", "set", "Master", strconv.Itoa(volume) + "%"}
+	}
+	return []string{"pactl", "set-sink-volume", "@DEFAULT_SINK@", strconv.Itoa(volume) + "%"}
+}
+
+// SetSpeakerVolume Sets the volume of the speaker to the provided value
+func SetSpeakerVolume(v int) (int, error) {
+	if outOfRange(v) {
+		return 0, errors.New("Value out of range")
+	}
+	_, err := execCmd(setVolumeCmd(v))
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+func parseVolume(v string) (int, error) {
+	lines := strings.Split(v, "\n")
+	for _, line := range lines {
+		str := strings.TrimLeft(line, " \t")
+
+		if (useAmixer && strings.Contains(str, "Playback") && strings.Contains(str, "%")) || !useAmixer && strings.HasPrefix(str, "Volume:") {
+			volStr := pattern.FindString(str)
+			return strconv.Atoi(volStr[:len(volStr)-1])
+		}
+	}
+	return 0, errors.New("No volume found")
+}
+
+func changeVolumeCmd(v int, sign string) []string {
+	if useAmixer {
+		return []string{"amixer", "set", "Master", strconv.Itoa(v) + "%" + sign}
+	}
+	return []string{"pactl", "--", "set-sink-volume", "@DEFAULT_SINK@", sign + strconv.Itoa(v) + "%"}
+}
+
+// IncreaseVolume Increase the volume by the provided value
+func IncreaseVolume(v int) (int, error) {
+	if outOfRange(v) {
+		return 0, errors.New("Value out of range")
+	}
+	_, err := execCmd(changeVolumeCmd(v, "+"))
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+// DecreaseVolume Decrese the volume by the provided value
+func DecreaseVolume(v int) (int, error) {
+	if outOfRange(v) {
+		return 0, errors.New("Value out of range")
+	}
+	_, err := execCmd(changeVolumeCmd(v, "-"))
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
 }
 
 func outOfRange(val int) bool {
