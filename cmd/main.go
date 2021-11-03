972bbf8999af92b431267b829a6d2d07874d30fd
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-11-03 17:06:33 +0100

    Main setup
    This is will be the setup to test the python interface

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,27 @@ import(
 	"fmt"
 	cv "gocv.io/x/gocv"
 	"os"
+	//"os/exec"
 )
 
+func toString(mat cv.Mat) (string) {
+	img, _ := mat.ToImage()
+	bounds := img.Bounds()
+	stringMatrix := ""
+
+	//don't get surprised of reversed order everywhere below
+	for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
+		for i := bounds.Min.X; i < bounds.Max.X; i++ {
+			r, g, b, _ := img.At(i, j).RGBA()
+			stringMatrix += string(byte(b>>8)) + string(byte(g>>8)) + string(byte(r>>8))	// ! absolutly too slow
+		}
+	}
+	return stringMatrix
+}
+
 func main() {
+
+	//pythonPath := "import os, sys;currentdir = os.path.dirname(os.path.realpath(os.getcwd()));parentdir = os.path.dirname(currentdir);sys.path.append(parentdir);from pkg.pythonGoInterface import pythonfile"	// I don't really know how i wrote this
 
 	// open webcam
 	webcam, err := cv.OpenVideoCapture(0)
@@ -31,7 +49,19 @@ func main() {
 			continue
 		}
 
-		
+		myString := toString(img)
+		fmt.Print("ciao")
+		fmt.Printf("\n-%s\n\n", myString[0:9])
+
+
+		/*
+		TODO this code is perfect, need to be finished
+		cmd := exec.Command("python.exe",  "-c", pythonPath+"; print(pythonGoInterface.myFunction("+"))")
+		out, err := cmd.CombinedOutput()
+    	if err != nil { fmt.Println(err); }
+    	fmt.Println(string(out))*/
+
+		os.Exit(3)
 
 		// show the image in the window, and wait 1 millisecond
 		window.IMShow(img)
